b92b0aff1ced1e557a885960b729f773dd952244
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2026-07-17 16:15:58 +0200

    node: Do not take global lock when joining

--- src/classy_node.erl
+++ src/classy_node.erl
@@ -457,20 +457,7 @@ handle_join(S, Call) ->
             ExpectedCluster =:= any ->
       case classy_hook:all(?on_pre_join, [Cluster, Remote, Node, Intent]) of
         ok ->
-          Res =
-            global:trans(
-              {classy_node_join_lock, self()},
-              fun() ->
-                  do_join_node(Node, Cluster, Remote, MemData, Intent, S)
-              end,
-              [node(), Node],
-              1),
-          case Res of
-            aborted ->
-              {error, aborted};
-            _ ->
-              Res
-          end;
+          do_join_node(Node, Cluster, Remote, MemData, Intent, S);
         {error, _} = Err ->
           Err
       end;
